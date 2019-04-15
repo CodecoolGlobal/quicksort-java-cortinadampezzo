cf8638c22d09a51fecc7bf54fffc79e3279ccbf8
cortinadampezzo <kisgans@gmail.com>
2019-04-15 07:31:29 +0200

    finished

--- src/main/java/com/codecool/quicksort/Main.java
+++ src/main/java/com/codecool/quicksort/Main.java
@@ -1,9 +1,19 @@
 package com.codecool.quicksort;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        // Use the tests to check your code!
+        List<Integer> numbers = Arrays.asList(10, 6, 5, 2, 3, 1, 4, 7, 9, 8);
+
+        QuickSort quickSort = new QuickSort();
+        quickSort.sort(numbers);
+
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
     }
 
 }
--- src/main/java/com/codecool/quicksort/QuickSort.java
+++ src/main/java/com/codecool/quicksort/QuickSort.java
@@ -4,11 +4,39 @@ import java.util.List;
 
 public class QuickSort {
 
-    /**
-     * Sorts the given List in place
-     * @param toSort the List to sort. Throws an error if its null
-     */
+    private void quickSort(List<Integer> toSort, int leftIndex, int rightIndex) {
+
+        int leftNumber = leftIndex;
+        int rightNumber = rightIndex;
+
+        while (toSort.get(leftNumber) < toSort.get(leftIndex + (rightIndex - leftIndex) / 2)) {
+            leftNumber++;
+        }
+
+        while (toSort.get(rightNumber) > toSort.get(leftIndex + (rightIndex - leftIndex) / 2)) {
+            rightNumber--;
+        }
+
+        int temporaryLeft = toSort.get(leftNumber);
+        int temporaryRight = toSort.get(rightNumber);
+        toSort.set(leftNumber, temporaryRight);
+        toSort.set(rightNumber, temporaryLeft);
+        leftNumber++;
+        rightNumber--;
+
+
+        if (leftIndex < rightNumber) {
+            quickSort(toSort, leftIndex, rightNumber);
+        }
+        if (leftNumber < rightIndex) {
+            quickSort(toSort, leftNumber, rightIndex);
+        }
+    }
+
     public void sort(List<Integer> toSort) {
 
+        if (toSort == null) throw new IllegalArgumentException();
+        quickSort(toSort, 0, toSort.size() - 1);
     }
+
 }
